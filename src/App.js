2bb8e0a68b3d7b817a39f97d378894498c92b886
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-11-17 13:11:12 +0100

    implemented routing to the overview page and sidebar and topbar components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import { Sidebar, Topbar } from "./components";
+import { Overview } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Overview />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
