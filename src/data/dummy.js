803e3ab3e5c6c3918c68c13f00e4262773a924d4
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-11-17 23:24:07 +0100

    created the widget data and table data

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -81,14 +81,187 @@ export const widgetData = [
   },
   {
     title: "Daily Transaction Value",
-    value: "2,342",
+    value: "#4,000,000",
   },
   {
     title: "Total Transaction Volume",
-    value: "2,342",
+    value: "452,000",
   },
   {
     title: "Total Transaction Volume",
-    value: "2,342",
+    value: "#4,000,000",
+  },
+];
+
+export const widgetChart = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+export const orderData = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+export const tableData = [
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "pending",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "reconciled",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "reconciled",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "pending",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "unreconciled",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "reconciled",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "unreconciled",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "reconciled",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "reconciled",
+  },
+  {
+    pic: "ww",
+    productName: "Apple Mac Book 15' 250 SSD 12GB",
+    price: "$73430",
+    transaction_no: "1234567890",
+    time: "12:30",
+    status: "reconciled",
   },
 ];
