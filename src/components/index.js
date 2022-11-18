5dd0b0e975225c59b8092ca5b2f5a3ffc600e0d4
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-11-18 01:10:09 +0100

    added the payment and order export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Topbar } from "./Topbar/Topbar";
 export { default as Widget } from "./Widget/Widget";
+export { default as Orders } from "./Orders/Orders";
+export { default as Payments } from "./Payments/Payments";
